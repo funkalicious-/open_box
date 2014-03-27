51a2f331fad72ecf5c0e1b17a122e5f7d278a37a
TheTastyCow <040916399@MY.CENTRAL.WA.EDU.AU>
2014-03-27 12:15:30 +0800

    asdf

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -2,7 +2,9 @@
 
    
 
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Thomas Kramer")
         ListBox1.Items.Add("cody")
         ListBox1.Items.Add("Salmaan")
         ListBox1.Items.Add("Silvana")
